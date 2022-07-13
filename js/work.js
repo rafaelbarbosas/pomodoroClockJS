7c12dd12a03129fc7caef061df9142a933d9566f
rafaelbarbosas <rafael.1940.b@gmail.com>
2022-07-13 18:01:03 -0300

    feat: add screen to manage work time

---
+++ js/work.js
@@ -0,0 +1,11 @@
+var workTime = null;
+var breakTime = null;
+var hasPauses = null;
+
+window.onload = () => {
+    // get the parameters from the URL
+    var urlParams = new URLSearchParams(window.location.search);
+    workTime = Number.parseInt(urlParams.get("workTime"));
+    breakTime = Number.parseInt(urlParams.get("breakTime"));
+    hasPauses = urlParams.get("hasPauses") == 'true';
+}
